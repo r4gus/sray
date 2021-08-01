247b9b4215ac9213ca19e832c210ece91bd94b37
David P. Sugar <foobarc11@protonmail.ch>
2021-08-01 12:19:17 +0200

    Basic operations for Point3 and Vector3 implemented

--- src/math.rs
+++ src/math.rs
@@ -217,19 +217,140 @@ impl Vector3 {
         Self(Tuple4 { x, y, z, w: 0.0 })
     }
 
-    /// Get the x coordinate of the given point.
+    /// Get the x coordinate of the given vector.
     pub fn x(&self) -> f64 {
         self.0.x
     }
 
-    /// Get the y coordinate of the given point.
+    /// Get the y coordinate of the given vector.
     pub fn y(&self) -> f64 {
         self.0.y
     }
 
-    /// Get the z coordinate of the given point.
+    /// Get the z coordinate of the given vector.
     pub fn z(&self) -> f64 {
         self.0.z
+    }
+    
+    /// Calculate the magnitude (length) of the given vector.
+    ///
+    /// Vectors with a magnitude of 1 are called unit vectors.
+    /// One can transform an arbitrary vector into a unit
+    /// vector using the `norm` method.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use sray::math::Vector3;
+    ///
+    /// let v1 = Vector3::new(1.0, 0.0, 0.0);
+    /// assert_eq!(1.0, v1.mag());
+    ///
+    /// let v2 = Vector3::new(0.0, 1.0, 0.0);
+    /// assert_eq!(1.0, v2.mag());
+    ///
+    /// let v3 = Vector3::new(0.0, 0.0, 1.0);
+    /// assert_eq!(1.0, v3.mag());
+    ///
+    /// let v4 = Vector3::new(1.0, 2.0, 3.0);
+    /// assert_eq!((14.0_f64).sqrt(), v4.mag());
+    ///
+    /// let v5 = Vector3::new(-1.0, -2.0, -3.0);
+    /// assert_eq!((14.0_f64).sqrt(), v5.mag());
+    /// ```
+    pub fn mag(&self) -> f64 {
+        (self.0.x.powi(2) + 
+         self.0.y.powi(2) +
+         self.0.z.powi(2) +
+         self.0.w.powi(2))
+            .sqrt()
+    }
+    
+    /// Normalize the given vector.
+    ///
+    /// A normalized vector has a magnitude of 1. This helps
+    /// to keep calculations anchored relative to a common
+    /// scale.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use sray::math::Vector3;
+    ///
+    /// let v = Vector3::new(4.0, 0.0, 0.0);
+    /// assert_eq!(Vector3::new(1.0, 0.0, 0.0), v.norm());
+    ///
+    /// let v = Vector3::new(1.0, 2.0, 3.0);
+    /// assert_eq!(Vector3::new(
+    ///     1.0 / 14.0_f64.sqrt(), 
+    ///     2.0 / 14.0_f64.sqrt(), 
+    ///     3.0 / 14.0_f64.sqrt()), v.norm());
+    ///
+    /// let v = Vector3::new(1.0, 2.0, 3.0);
+    /// assert_eq!(1.0, v.norm().mag());
+    /// ```
+    pub fn norm(self) -> Self {
+        let mv = self.mag();
+
+        Self(Tuple4 {
+            x: self.0.x / mv,
+            y: self.0.y / mv,
+            z: self.0.z / mv,
+            w: self.0.w / mv
+        })
+    }
+    
+    /// Calculate the dot product of two vectors.
+    ///
+    /// The dot product (scalar product) is the sum of the
+    /// products of the corresponding components of each
+    /// vector and represents the cosine of the angle between
+    /// the two vectors if both are unit vectors, i.e. the
+    /// smaller the dot product, the larger the angle between
+    /// the vectors.
+    ///
+    /// A dot product of 1 means the vectors are identical and
+    /// a cosine of -1 means they point in opposite directions.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use sray::math::Vector3;
+    ///
+    /// let v1 = Vector3::new(1.0, 2.0, 3.0);
+    /// let v2 = Vector3::new(2.0, 3.0, 4.0);
+    /// 
+    /// assert_eq!(20.0, v1.dot(&v2));
+    /// ```
+    pub fn dot(&self, _rhs: &Self) -> f64 {
+        self.0.x * _rhs.0.x +
+        self.0.y * _rhs.0.y +
+        self.0.z * _rhs.0.z +
+        self.0.w * _rhs.0.w
+    }
+    
+    /// Calculate the cross product of two vectors.
+    ///
+    /// The resulting vector is prependicular to both of
+    /// the original vectors.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use sray::math::Vector3;
+    ///
+    /// let v1 = Vector3::new(1.0, 2.0, 3.0);
+    /// let v2 = Vector3::new(2.0, 3.0, 4.0);
+    ///
+    /// assert_eq!(Vector3::new(-1.0, 2.0, -1.0), v1.cross(&v2));
+    /// assert_eq!(Vector3::new(1.0, -2.0, 1.0), v2.cross(&v1));
+    /// ```
+    pub fn cross(&self, _rhs: &Self) -> Self {
+        Self::new(
+            self.y() * _rhs.z() - self.z() * _rhs.y(),
+            self.z() * _rhs.x() - self.x() * _rhs.z(),
+            self.x() * _rhs.y() - self.y() * _rhs.x()
+        )
     }
 }
 
